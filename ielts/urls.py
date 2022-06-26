6b83448200e360723b68cbfc582724b7ee35fc1d
Balogun Sodiq <sodiqb86@gmail.com>
2022-06-26 12:18:02 -0700

    initial commit

--- ielts/urls.py
+++ ielts/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path as url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
