4802893ea596311cfb4e3746c75dbc75ecdbedb2
Balogun Sodiq <sodiqb86@gmail.com>
2022-06-26 12:11:18 -0700

    initial commit

--- ielts/settings.py
+++ ielts/settings.py
@@ -107,7 +107,7 @@ DATABASES['default'].update(db_from_env)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
